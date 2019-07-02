1c3af75461483724c1cfad2cd61bace77c084b7d
Shereal McNeal <opcommander1@hotmail.com>
2019-07-01 21:45:49 -0500

    add footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <Navbar v-bind:localusername="localusername"/>
     <router-view v-bind:Lessons="Lessons" />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/layout/Footer'
 // import Royalplayer from '@/components/Royalplayer'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
   data(){
     return {
---
+++ client/src/components/layout/Footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="Footer">
+    <footer class="page-footer purple lighten-3">
+      <div class="container">
+        <div class="row">
+          <div class="col m12 s12">
+            <h5 class="black-text">Social Media Links</h5>
+          </div>
+          <div class="col m12 s12">
+            <ul>
+              <li><a class="black-text" href="">Facebook</a></li>
+              <li><a class="black-text" href="">Twitter</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container black-text">
+          @ 2019 copyright
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+  .Footer{
+    text-align: center;
+  }
+
+  .Footer h5{
+    font-weight: 400;
+  }
+
+  .Footer li a{
+    font-weight: 300;
+  }
+
+  .footer-copyright{
+    font-weight: 300;
+  }
+</style>
+
+
