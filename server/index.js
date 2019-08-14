d421940b09185aff24ae5bffdaaae33141963936
Shereal McNeal <opcommander1@hotmail.com>
2019-08-13 20:58:19 -0500

    update comment

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ passport.use(new LocalStrategy(
   }
 ));
 
-//intializing passport for user athenication for session
+//intializing passport for user athenication
 app.use(passport.initialize());
 
 
@@ -34,12 +34,3 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started at ${port}`));
 
-// con.query("SELECT * FROM Signup", function (err, result, fields){
-//   console.log(result);
-// })
-
-// var sql = "INSERT INTO Signup (first_name, last_name, email, username, password) VALUES ('Shereal', 'McNeal', 'smcneal@hotmail.com', 'smcneal', 'fleet')";
-// con.query(sql, function (err, result) {
-//   if (err) throw err;
-//   console.log("record added")
-// })
