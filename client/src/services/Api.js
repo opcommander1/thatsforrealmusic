5d73b0380706780d23f2984fb768cb6f189e0629
Shereal McNeal <opcommander1@hotmail.com>
2019-08-17 16:13:16 -0500

    update client

--- client/src/services/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default () => {
-  return axios.create({
-    //set the url for the api server location on port 5000
-    baseURL: `http://localhost:5000/`
-  })
-}
