e223756c7539e53e2472668dee9b939a98fa90df
Shereal McNeal <opcommander1@hotmail.com>
2019-08-17 21:35:35 -0500

    cleardb

--- server/modules/config.js
+++ server/modules/config.js
@@ -1,10 +1,11 @@
 let mysql = require('mysql')
 
 let con = mysql.createConnection({
-  host: 'mysql.op1commander.com',
-  user: 'op1commander',
-  password: 'fleetofficer50',
-  database: 'forrealmusic',
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+  user: 'be0d3552fd5dc7',
+  password: '1699e848',
+  database: 'heroku_83a0630020d897f',
+  // mysql://be0d3552fd5dc7:1699e848@us-cdbr-iron-east-02.cleardb.net/heroku_83a0630020d897f?reconnect=true
 })
 
 con.connect((err) => {
